ee58258272595a37bd5956e06cd3ee7682b00c0c
Isaiah Anthony <isaiah.anthony99@gmail.com>
2021-01-17 15:57:33 -0800

    schema and index db

---
+++ models/workout.js
@@ -0,0 +1,46 @@
+const {model, Schema} = require('mongoose')
+const { Workout } = require('.')
+
+const Plan = new Schema({
+  day: {
+type: Date,
+default: () => new Date()
+  },
+  exercises:[
+    {
+      type: {
+        type: String,
+      trim: true,
+      required: 'Enter an excercise type'
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: 'Enter an exercise name'
+      },
+      duration: {
+        type: Number,
+        trim: true,
+        required: 'Enter a duration'
+      },
+      weight: {
+        type: Number,
+        trim: true,
+        required: 'Enter a weight'
+      },
+      reps: {
+        type: Number,
+        trim: true,
+        required: 'Enter an amount of reps'
+      },
+      sets: {
+        type: Number,
+        trim: true,
+        required: 'Enter an amount of sets'
+      }
+    }
+  ]
+})
+
+
+module.exports = model('Workout', Workout)
